6f4f9b25a8bd301e74c2b05b3781764f458421a3
mikirui <545559910@qq.com>
2016-04-30 00:30:17 +0800

    norm_cuda with BGR

---
+++ norm_cuda.lua
@@ -0,0 +1,22 @@
+local norm_cuda, parent = torch.class("nn.norm_cuda", "nn.Module") -- NOTES: return metatables
+
+function norm_cuda:__init()
+   self.size = 384
+end
+
+-- a bandwidth limited sensor which focuses on a location.
+-- locations index the x,y coord of the center of the output glimpse
+function norm_cuda:updateOutput(input)
+   self.output = input:cuda()
+   local mean = torch.CudaTensor(input:size())
+   mean[{{}, {1}, {}, {}}] = -104
+   mean[{{}, {2}, {}, {}}] = -117
+   mean[{{}, {3}, {}, {}}] = -123
+   self.output:mul(255):add(mean)
+   --print(self.output:size())
+   return self.output
+end
+
+function norm_cuda:updateGradInput(inputTable, gradOutput)
+--   local input = inputTable
+end
