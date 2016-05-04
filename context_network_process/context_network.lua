115a916d49e4c3be971121d719b9fb675388a5be
mikirui <545559910@qq.com>
2016-05-04 15:28:38 +0800

    modify context network to fit 256 input

--- context_network_process/context_network.lua
+++ context_network_process/context_network.lua
@@ -1,23 +1,14 @@
 require 'torch'
 require 'nn'
-require 'inn'
-require 'nn'
-require 'cutorch'
-require 'cunn'
-require 'cudnn'
 require 'hdf5'
-require 'dp'
-require 'dpnn'
-require 'optim'
-require 'norm_cuda'
-require 'norm_cuda_con'
+require 'nnx'
 
-h5_file_path = '/home/qianlima/torch-test/DRAM/dataset/MSCOCO/coco384/cocotalk384.h5'
-ft_matrix_path = '/home/qianlima/torch-test/DRAM/dataset/MSCOCO/coco384/cocotalk384_ft_matrix.t7'
---h5_file_path = '/home/qianlima/torch-test/DRAM/dataset/Flickr8k/f8k384/flickr8ktalk384.h5'
---ft_matrix_path = '/home/qianlima/torch-test/DRAM/dataset/Flickr8k/f8k384/flickr8ktalk384_ft_matrix.t7'
---h5_file_path = '/home/qianlima/torch-test/DRAM/dataset/Flickr30k/f30k384/flickr30ktalk384.h5'
---ft_matrix_path = '/home/qianlima/torch-test/DRAM/dataset/Flickr30k/f30k384/flickr30ktalk384_ft_matrix.t7'
+--h5_file_path = '/home/qianlima/torch-test/DRAM/dataset/MSCOCO/coco256/cocotalk256.h5'
+--ft_matrix_path = '/home/qianlima/torch-test/DRAM/dataset/MSCOCO/coco256/cocotalk256_ft_matrix.t7'
+h5_file_path = '/home/qianlima/torch-test/DRAM/dataset/Flickr8k/f8k256/flickr8ktalk256.h5'
+ft_matrix_path = '/home/qianlima/torch-test/DRAM/dataset/Flickr8k/f8k256/flickr8ktalk256_ft_matrix.t7'
+--h5_file_path = '/home/qianlima/torch-test/DRAM/dataset/Flickr30k/f30k256/flickr30ktalk256.h5'
+--ft_matrix_path = '/home/qianlima/torch-test/DRAM/dataset/Flickr30k/f30k256/flickr30ktalk256_ft_matrix.t7'
 model_path = '/home/qianlima/torch-test/raw_googlenet.t7'
 
 ---1.read h5 file to get image file information---
@@ -28,21 +19,29 @@ local num_channels = file_size[2]
 local image_size = file_size[3]
 local batch_size = 30
 local cur = 1
-ft_matrix = torch.CudaTensor(num_images,1024):fill(0)   --feature matrix,each images 1024 features
-
 
 ---2.define the model(sample & googlenet)------
+res = nn.SpatialReSampling{oheight=96,owidth=96}   --SpatialAveragePooling
+
+require 'inn'
+require 'cutorch'
+require 'cunn'
+require 'cudnn'
+require 'dp'
+require 'dpnn'
+require 'optim'
+require 'norm_cuda'
+require 'norm_cuda_con'
+
 cnn = torch.load(model_path)
 model = nn.Sequential()
 model:add(nn.norm_cuda_con())    --norm_cuda
-model:add(cudnn.SpatialAveragePooling(4,4,4,4))    --SpatialAveragePooling
 model:add(cnn)    --googlenet
 
 model = model:cuda()
 model:evaluate()
 
---output = model:forward(input:cuda())
-
+ft_matrix = torch.CudaTensor(num_images,1024):fill(0)
 
 ---3.main loop to process the data------
 for i = 1,num_images,batch_size
@@ -50,7 +49,8 @@ do
 	ed = math.min(cur + batch_size - 1,num_images)
 	local imgs = h5_file:read('/images'):partial({cur,ed},{1,num_channels},
                             {1,image_size},{1,image_size})
-	ft = model:forward(imgs:cuda())    ---get the feature through googlenet
+	sa = res:forward(imgs:double())             --resize to 96*96
+	ft = model:forward(sa:cuda())    ---get the feature through googlenet
 	ft_matrix[{{cur,ed}}] = ft
 	print(string.format('solved images from %d to %d\n',cur,ed))
 	cur = ed + 1
